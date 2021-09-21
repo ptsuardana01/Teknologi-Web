beba6b93252ad1ed53655df858bf8af2edaa1456
ptsuardana01 <ptsuadana01@gmail.com>
2021-09-21 13:09:32 +0800

    - Menyempurnakan halaman homeBody.vue dan Navbar.vue - Menambah png profil.png dan profil-squre.png

--- tugas-vue1/src/components/Home/homeBody.vue
+++ tugas-vue1/src/components/Home/homeBody.vue
@@ -1,9 +1,18 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      FrontEnd Development
-    </p>
+  <div id="container" class="bg-gradient-to-tr from-indigo-100 flex justify-evenly items-center">
+    <div class="profil-home bg-gradient-to-l from-green-300 to-blue-500 rounded-full">
+      <img class="rounded-full h-96" src="../../assets/profil-squre.png" alt="psDevelopmen">
+    </div>
+
+    <div class="desc-home w-96">
+      <h1 class="font-bold text-3xl">{{ msg }}</h1>
+      <p class="text-sm pb-8">FrontEnd Development</p>
+      <p class="break-word">Lorem ipsum dolor sit amet consectetur adipisicing elit. Explicabo atque, laudantium nihil distinctio vero 
+        itaque velit voluptates dicta optio eos quisquam illum ipsa ut modi autem ducimus reprehenderit aperiam 
+        voluptate.</p>
+    </div>
+
+
   </div>
 </template>
 
@@ -15,4 +24,10 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+#container {
+  height: 90vh;
+}
+</style>
 
--- tugas-vue1/src/components/Navbar.vue
+++ tugas-vue1/src/components/Navbar.vue
@@ -1,12 +1,12 @@
 <template>
-    <nav id="nav" class="h-16 bg-blue-200 flex justify-between p-4 px-16 shadow-md">
+    <nav id="nav" class="h-14 bg-gradient-to-l from-green-300 to-blue-500 flex justify-between p-3 px-16 shadow-md">
         <div class="flex justify-center">
             <span class="logo ">psWebsite</span>
         </div>
         <div class="nav-menu w-80 flex justify-around">
-            <router-link class="font-medium px-3" to="/">Home</router-link>
-            <router-link class="font-medium px-3" to="/about">About</router-link>
-            <router-link class="font-medium px-3" to="/keranjang-belanja">Keranjang Belanja</router-link>
+            <router-link class="font-medium px-2" to="/">Home</router-link>
+            <router-link class="font-medium px-2" to="/about">About</router-link>
+            <router-link class="font-medium px-2" to="/keranjang-belanja">Keranjang Belanja</router-link>
         </div>
   </nav>
 </template>
